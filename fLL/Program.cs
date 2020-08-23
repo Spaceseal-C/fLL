83070103cdcf8b9c3c735ed8bdfa3f7a397c6d60
Spaceseal-C <63883200+Spaceseal-C@users.noreply.github.com>
2020-08-23 18:40:35 +0200

    Add files via upload

---
+++ fLL/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace fLL
+{
+
+    class fLL
+    {
+        static Random r = new Random();
+        static void Main(string[] args)
+        {
+            String fnev = "";
+            Console.Write("Kérem  a beolvasandó file nevét:");
+            fnev = Console.ReadLine(); //a file nevét kéri be amiben el van választva a szöveg.
+            Console.Clear();
+            int n = 0;
+            n = nu(n, fnev);
+            bool meow = false;
+            if (ellenorzes(n, meow))
+            {
+                String[,] szavak = new String[n, 2];
+                int e = n; //random szám
+                szavak = m(szavak, fnev);
+                while (true)
+                {
+                    e = kivalaszt(e, n); //randomizálás
+                    Console.WriteLine(szavak[e, 0]);
+                    String s = "";
+                    while (s != szavak[e, 1])
+                    {
+                        s = Console.ReadLine();
+                    }
+                    Console.Clear();
+                }
+            }
+        }
+        static int nu(int nm, string fnev)
+        {
+            
+            StreamReader sr = new StreamReader(fnev + ".txt");
+            while (!sr.EndOfStream)
+            {
+                sr.ReadLine();
+                nm += 1;
+            }
+            return nm;
+        }
+        static string[,] m(String[,] magyara, string fnev)
+        {
+            StreamReader sr = new StreamReader(fnev + ".txt");
+            List<string> magyar = new List<string>();
+            while (!sr.EndOfStream)
+            {
+                magyar.Add(sr.ReadLine());
+            }
+            int cal = 0; //segedvaltozo, megszamlalja, hogy hanyadik elem jon a tombben
+            string[] magy = new string[2]; 
+            foreach (string item in magyar)
+            {
+                magy = item.Split(';');
+                magyara[cal, 0] = magy[0];
+                magyara[cal, 1] = magy[1];
+                cal++;
+            }
+            return magyara;
+        }
+        static bool ellenorzes(int n, bool meow) //ellenőrzi, hogy a fileban van-e szöveg.
+        {
+            meow = true;
+            if (n == 0)
+            {
+                Console.WriteLine("A fileban nem található szöveg.");
+                meow = false;
+                Console.ReadLine();
+            }
+            return meow;
+        }
+        static int kivalaszt(int s, int h)
+        {
+            s = r.Next(0, h);
+            return s;
+        }
+    }
+}
---
+++ fLL/README.txt
@@ -0,0 +1,18 @@
+Created by SpaceSeal in 2020.08.23
+Posted: 2020.08.23
+Project name: fLL
+
+A projekt egy forráskódot, egy exe file-t, egy README file-t és egy 5 példából álló magyar-angol forrást tartalmaz.
+
+Ez a projekt még mindig fejlesztés alatt van.
+Ha hibát találsz kérlek jelezd.
+
+EZ A PROGRAM NEM ÁRUSÍTHATÓ!
+
+Hogyan használd:
+    A forrásfileba ";"-el elválasztva írd be a szavakat így:"mutatott_nyelv;bekért nyelv". pl.:(igaz;true)
+    Indítsd el a programot.
+    A program bekéri a beolvasandó file nevét.
+        A file kiterjesztését NEM kell megadni.
+    Majd a program addig kéri be a választ ameddig a helyes választ meg nem adod.
+    A program végtelenített ciklusban fut, be kell zárni, ahoz, hogy megálljon.
